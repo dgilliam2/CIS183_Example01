ec54e119c481508a6fcf3fefd214dacc379bbc5f
dgilliam2 <165728152+dgilliam2@users.noreply.github.com>
2024-08-28 15:04:28 -0400

    8/28/2024
    8/28/2024

--- CIS183_Example01/app/src/main/java/com/example/cis183_example01/MainActivity.java
+++ CIS183_Example01/app/src/main/java/com/example/cis183_example01/MainActivity.java
@@ -1,28 +1,42 @@
 package com.example.cis183_example01;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+//all vars will be global
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView lbl_j_HW;
+    EditText etxt_j_TB;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         //code here
+        //assign xml vars to java vars
+        lbl_j_HW = findViewById(R.id.lbl_v_HW);
+        etxt_j_TB = findViewById(R.id.etxt_v_TB);
+        setGreetingMessage();
+        String nameFromTextBox = getNameFromTextBox();
+        Log.d("The name is ",  nameFromTextBox);
+    }
 
+    public void setGreetingMessage()
+    {
+        lbl_j_HW.setText("Welcome to app");
+    }
 
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+    public String getNameFromTextBox()
+    {
+        return etxt_j_TB.getText().toString();
     }
 }
